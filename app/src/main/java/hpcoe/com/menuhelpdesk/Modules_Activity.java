51462513103acc0cc742549b958ee13bf11b0cd2
sanjay kumar <sanjaykumara1994@gmail.com>
2015-05-25 16:27:49 +0530

    add dynamic list view

--- app/src/main/java/hpcoe/com/menuhelpdesk/Modules_Activity.java
+++ app/src/main/java/hpcoe/com/menuhelpdesk/Modules_Activity.java
@@ -8,8 +8,11 @@ import android.content.SharedPreferences;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
 
@@ -21,8 +24,14 @@ import java.util.Map;
 
 public class Modules_Activity extends ActionBarActivity {
 
+    private ListView searchFilter;
+    private EditText searchEdit;
+    private ArrayList<String> stringList;
+    private ValueAdapter valueAdapter;
+    private TextWatcher searchtw;
+
     SharedPreferences settings;
-    List<Map<String, String>> modulesList = new ArrayList<Map<String, String>>();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,72 +39,47 @@ public class Modules_Activity extends ActionBarActivity {
         settings=getSharedPreferences("loginPrefs", Context.MODE_PRIVATE);
 
         initList();
+        initUI();
 
-        Toolbar toolbar= (Toolbar) findViewById(R.id.app_bar);
-        setSupportActionBar(toolbar);
-        ListView lv = (ListView) findViewById(R.id.modules_list);
-
-        SimpleAdapter simpleAdapter = new SimpleAdapter(this, modulesList, android.R.layout.simple_list_item_1, new String[] {"ABCD"}, new int[] {android.R.id.text1});
-        lv.setAdapter(simpleAdapter);
+        valueAdapter = new ValueAdapter(stringList, this);
+        searchFilter.setAdapter(valueAdapter);
+        searchEdit.addTextChangedListener(searchtw);
     }
 
     private void initList() {
-        modulesList.add(createModule("ABCD", "abcd"));
-        modulesList.add(createModule("ABCD", "efgh"));
-        modulesList.add(createModule("ABCD", "ijkl"));
-        modulesList.add(createModule("ABCD", "mnop"));
-        modulesList.add(createModule("ABCD", "qrst"));
+        stringList = new ArrayList<String>();
+        stringList.add("one");
+        stringList.add("two");
+        stringList.add("three");
+        stringList.add("four");
+        stringList.add("five");
+        stringList.add("six");
+        stringList.add("seven");
+        stringList.add("eight");
+        stringList.add("nine");
+        stringList.add("ten");
+
+        searchtw = new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+                valueAdapter.getFilter().filter(s);
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                valueAdapter.getFilter().filter(s);
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                valueAdapter.getFilter().filter(s);
+            }
+        };
     }
 
-    private HashMap<String, String> createModule(String key, String val) {
-        HashMap<String, String> module = new HashMap<String, String>();
-        module.put(key, val);
-        return module;
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_modules_, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        if(id==R.id.action_logout){
-            AlertDialog.Builder alert=new AlertDialog.Builder(this);
-            alert.setTitle("Logout");
-            alert.setMessage("Do you want to logout?");
-            alert.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
-                @Override
-                public void onClick(DialogInterface dialog, int which) {
-                    SharedPreferences.Editor editor = settings.edit();
-                    editor.putBoolean("isLoggedIn", false);
-                    editor.apply();
-                    startActivity(new Intent(getApplicationContext(), SignIn.class));
-                    finish();
-                }
-            });
-            alert.setNegativeButton("No",new DialogInterface.OnClickListener() {
-                @Override
-                public void onClick(DialogInterface dialog, int which) {
-
-                }
-            });
-            alert.show();
-        }
-
-        return super.onOptionsItemSelected(item);
+    private void initUI() {
+        searchFilter = (ListView) findViewById(R.id.modules_list);
+        searchEdit = (EditText) findViewById(R.id.searchedt);
     }
 
 
